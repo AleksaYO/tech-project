fe01228be64fe0ad4b033695679bdf85bc84ea26
AleksaYO <AleksaYO@users.noreply.github.com>
2023-05-18 15:58:19 +0000

    deploy: 05964af9d5d79bbdc26d92d483a06f8969a00f0e

---
+++ assets/HomePage-bf66bbe8.js
@@ -0,0 +1,1 @@
+import{j as r,L as s}from"./index-73506985.js";const t=()=>r.jsxs("div",{children:["return ",r.jsx(s,{to:"/cards",children:"Cards"}),r.jsx("p",{children:"XYU"})]});export{t as default};
